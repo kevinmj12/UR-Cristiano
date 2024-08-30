81165ff9aa53f6b1ff3fabcbac33ac4e710c197b
kevinmj12 <kevinmj12@gmail.com>
2024-08-30 15:34:59 +0900

    Q3-숫자야구
    민제-유저 입력에 따른 결과 출력 구현

---
+++ Q3.py
@@ -0,0 +1,54 @@
+import random
+
+print("숫자 야구 게임을 시작합니다.")
+
+while True:
+
+    answer = random.randrange(100, 1000)
+    answer = str(answer)
+    errorCount = 0
+
+    while(errorCount < 3):
+        flag = False
+        try:
+            num = int(input("숫자를 입력해주세요 : "))
+            if (num < 100 or num > 999):
+                errorCount += 1
+                continue
+
+            strike = 0
+            ball = 0
+            strNum = str(num)
+            for i in range(0, 3):
+                for j in range(0, 3):
+                    if (answer[i] == strNum[j]):
+                        if (i == j):
+                            strike += 1
+                        else:
+                            ball += 1
+                        continue
+            
+            if (strike == 0 and ball == 0):
+                print("OUT")
+            elif (strike == 3):
+                print("3개의 숫자를 모두 맞히셨습니다! 게임이 종료됩니다!")
+                next = input()
+                
+                if next == "Y":
+                    break
+                else:
+                    flag = True
+                    break
+            else: 
+                if (ball > 0):
+                    print(str(ball)+"B", end="")
+                if (strike > 0):
+                    print(str(strike)+"S", end="")
+                print("\n")
+            
+
+        except:
+            errorCount += 1
+
+        if (flag):
+            break
